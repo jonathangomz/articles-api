e4794466277dd8ba4c58505d983c18a6e65af3cc
jonathangomz <jonicgp97@gmail.com>
2024-02-29 13:49:06 -0600

    Updates get user endpoint to return only authenticated user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards, Req, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { NotFoundError } from 'rxjs';
 import { JwtAuthGuard } from 'src/services/auth/guards/jwt.guards';
@@ -9,11 +9,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':username')
-  async findOne(@Param('username') username: string) {
-    const user = await this.usersService.findOne(username);
-    if(!user) {
-      throw new NotFoundException(`User ${username} not found`);;
+  async findOne(@Req() request, @Param('username') username: string) {
+    const authenticatedUser = request.user;
+    if(authenticatedUser.username !== username) {
+      throw new UnauthorizedException();
     }
+
+    const user = await this.usersService.findOne(username);
     delete user.password;
     return user;
   }
