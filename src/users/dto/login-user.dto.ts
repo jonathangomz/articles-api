f951f482689694409d406d3f8099018f536208e5
jonathangomz <jonicgp97@gmail.com>
2024-03-01 23:43:46 -0600

    Removes unused import

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType, PickType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class LoginUserDto extends PickType(CreateUserDto, ['username', 'password'] as const) {}
